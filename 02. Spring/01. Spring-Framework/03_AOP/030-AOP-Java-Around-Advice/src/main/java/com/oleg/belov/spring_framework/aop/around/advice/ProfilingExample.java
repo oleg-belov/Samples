bc1b127a10d9257350d8e3a5ee923e6846019a5b
Oleg Belov <HiImOleg@gmail.com>
2017-05-07 18:50:59 +0300

    Add Around-Advice sample

---
+++ 02. Spring/01. Spring-Framework/03_AOP/030-AOP-Java-Around-Advice/src/main/java/com/oleg/belov/spring_framework/aop/around/advice/ProfilingExample.java
@@ -0,0 +1,20 @@
+package com.oleg.belov.spring_framework.aop.around.advice;
+
+import org.springframework.aop.framework.ProxyFactory;
+
+public class ProfilingExample {
+    public static void main(String[] args) {
+        WorkerBean bean = getWorkerBean();
+        bean.doSomeWork(1000);
+    }
+
+    private static WorkerBean getWorkerBean() {
+        WorkerBean target = new WorkerBean();
+
+        ProxyFactory factory = new ProxyFactory();
+        factory.setTarget(target);
+        factory.addAdvice(new ProfilingInterceptor());
+
+        return (WorkerBean)factory.getProxy();
+    }
+}
---
+++ 02. Spring/01. Spring-Framework/03_AOP/030-AOP-Java-Around-Advice/src/main/java/com/oleg/belov/spring_framework/aop/around/advice/ProfilingInterceptor.java
@@ -0,0 +1,44 @@
+package com.oleg.belov.spring_framework.aop.around.advice;
+
+import static com.oleg.belov.spring_framework.aop.around.advice.utils.ClassNameUtil.getCurrentClassName;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StopWatch;
+
+public class ProfilingInterceptor implements MethodInterceptor {
+	private static Logger log = LoggerFactory.getLogger(getCurrentClassName());
+	
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        StopWatch sw = new StopWatch();
+        sw.start(invocation.getMethod().getName());
+
+        Object returnValue = invocation.proceed();
+
+        sw.stop();
+        dumpInfo(invocation, sw.getTotalTimeMillis());
+        return returnValue;
+    }
+
+    private void dumpInfo(MethodInvocation invocation, long ms) {
+        Method m = invocation.getMethod();
+        Object target = invocation.getThis();
+        Object[] args = invocation.getArguments();
+
+       log.info("Executed method: " + m.getName());
+       log.info("On object of type: " + 
+                target.getClass().getName());
+
+       log.info("With arguments:");
+        for (int x = 0; x < args.length; x++) {
+            System.out.print("       > " + args[x]);
+        }
+        System.out.print("\n");
+
+       log.info("Took: " + ms + " ms");
+    }
+}
