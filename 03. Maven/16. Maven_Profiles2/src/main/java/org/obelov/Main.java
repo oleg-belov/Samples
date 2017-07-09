9f7efb141b30f2ddb78e072b977973f52923e542
Oleg Belov <obelov@249.42.168.192.in-addr.arpa>
2017-07-09 17:41:42 +0300

    Add Maven module

---
+++ 03. Maven/16. Maven_Profiles2/src/main/java/org/obelov/Main.java
@@ -0,0 +1,18 @@
+package org.obelov;
+
+
+public class Main{
+  
+    public static void main(String [] args){
+		
+		
+		for (String a: args){
+			
+			System.out.println(a);
+		
+		}
+		
+		
+    }
+
+}
