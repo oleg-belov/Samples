feea0df7258b74fa681b24bb9cf905ce18c49d00
Oleg Belov <Belov.Oleg.Sergeev@gmail.com>
2017-07-15 21:23:19 +0300

    Add another submodules in Spring-MVC module

--- 02. Spring/03. MVC/060_AutowiringControllerServices/src/main/java/com/oleg/belov/spring/mvc/controllers/ProjectController.java
+++ 02. Spring/03. MVC/060_AutowiringControllerServices/src/main/java/com/oleg/belov/spring/mvc/controllers/ProjectController.java
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.infiniteskills.mvc.data.services.ProjectService;
+import com.oleg.belov.spring.mvc.data.services.ProjectService;
 
 @Controller
 @RequestMapping("/project")
